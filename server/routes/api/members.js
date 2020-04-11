6035cc0b7cfb3d5d75f300c173a2e2448acdccb2
Theo Reyes <vincintz@yahoo.com>
2020-04-11 09:50:44 +0100

    Refactor: moved backend code to server. Prep for frontend, I hope.

--- server/routes/api/members.js
+++ server/routes/api/members.js
@@ -9,29 +9,26 @@ router.get('/', (req, res) => res.json(members));
 router.get('/:id', (req, res) => {
     const member = members.filter(m => m.id === parseInt(req.params.id));
     if (member.length == 0) {
-        res.status(404)
-           .json( { msg: `Member (id: ${req.params.id}) not found`} );
+        return res.status(404)
+                  .json( { msg: `Member (id: ${req.params.id}) not found`} );
     }
     else {
-        res.json(member);
+        return res.json(member);
     }
 });
 
 // Create a new member
 router.post('/', (req, res) => {
+    if (!req.body.name)
+        return res.status(400)
+                  .json( { msg: "You need to provide a name" } );
     const member = {
         id: Math.max(0, ...members.map(m => m.id)) + 1,
         name: req.body.name,
         age: req.body.age,
     };
-    if (!member.name) {
-        res.status(400)
-           .json( { msg: "You need to provide a name" } );
-    }
-    else {
-        members.push(member);
-        res.json( member );
-    }
+    members.push(member);
+    return res.json( member );
 });
 
 // Update a member
@@ -45,13 +42,13 @@ router.put('/:id', (req, res) => {
                   .json({ msg: "Please provide a member name" });
     const member = members.find(m => m.id === parseInt(req.params.id));
     if (!member) {
-        res.status(404)
-           .json( { msg: `Member with id = ${req.params.id} not found.` } );
+        return res.status(404)
+                  .json( { msg: `Member with id = ${req.params.id} not found.` } );
     }
     else {
         member.name = req.body.name;
         member.age = req.body.age;
-        res.json( { msg: "Update successful", member: member } );
+        return res.json( { msg: "Update successful", member } );
     }
 });
 
@@ -63,13 +60,13 @@ router.patch('/:id', (req, res) => {
                   .json({ msg: "Member id required" });
     const member = members.find(m => m.id === parseInt(req.params.id));
     if (!member) {
-        res.status(404)
-           .json( { msg: `Member with id = ${req.params.id} not found.` } );
+        return res.status(404)
+                  .json( { msg: `Member with id = ${req.params.id} not found.` } );
     }
     else {
         member.name = req.body.name ? req.body.name : member.name;
         member.age = req.body.age ? req.body.age : member.age;
-        res.json( { msg: "Update successful", member: member } );
+        return res.json( { msg: "Update successful", member } );
     }
 });
 
@@ -84,9 +81,8 @@ router.delete('/:id', (req, res) => {
         return res.status(404)
                   .json( { msg: `Member with id = ${req.params.id} not found.` } );
     const index = members.indexOf(member);
-    console.log(index, member);
     members.splice(index, 1);
-    return res.json({ msg: "Member deleted", member: member, all: members });
+    return res.json({ msg: "Member deleted", member });
 });
 
 module.exports = router;
