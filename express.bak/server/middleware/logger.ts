b5d7bf9c32064542bdeb2e70ac32561f461a68f7
Theo Reyes <vincintz@yahoo.com>
2020-05-14 09:02:58 +0100

    Replace with updated version

--- express.bak/server/middleware/logger.ts
+++
@@ -1,9 +0,0 @@
-import moment from 'moment';
-import { Request, Response, NextFunction } from 'express';
-
-const logger = (req: Request, res: Response, next: NextFunction) => {
-    console.log(`Request: ${req.protocol}://${req.get('host')}${req.originalUrl} ;; Time: ${moment().format()}`);
-    next();
-};
-
-export default logger;
